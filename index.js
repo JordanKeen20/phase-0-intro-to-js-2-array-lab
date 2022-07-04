752b366b82b86f0bd960bbf27e3818c1d860caf7
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-07-04 03:06:49 -0700

    commit tree 35cc125d20b8495c29c03b85b9365b4f32c556a6

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+
+function appendCat(){
+    const cuteCat = [...cats, 'Broom'];
+    return cuteCat;
+}
+
+function prependCat(){
+    let blueCat = ['Arnold', ...cats]
+    return blueCat;
+}
+
+function removeLastCat(){
+    let blackCat = cats.slice(0,2)
+    return blackCat;
+}
+
+function removeFirstCat(){
+    let yellowCat = cats.slice(1)
+    return yellowCat;
+}
